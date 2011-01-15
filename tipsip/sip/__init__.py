acee9275935fe815fdba01ff7d7cc50c09d52ef2
John Khvatov <ivaxer@imarto.net>
2011-01-15 04:40:33 +0300

    Updated __init__.py of sip package for recent changes

--- tipsip/sip/__init__.py
+++ tipsip/sip/__init__.py
@@ -1,7 +1,7 @@
 from uri import URI
 
 from header import Headers
-from header import AddressHeader, ViaHeader
+from header import Header, AddressHeader, ViaHeader, CSeqHeader
 
 from message import Message, Request, Response
 from message import MessageParsingError
@@ -10,3 +10,4 @@ from dialog import Dialog, DialogStore
 
 from transport import Address, UDPTransport
 
+from ua import SIPUA
