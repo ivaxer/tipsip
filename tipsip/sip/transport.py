216b7c21125b978f1b9d136565efd9502a43aedb
John Khvatov <ivaxer@imarto.net>
2011-01-15 04:03:26 +0300

    UDPTransport: implemented sendMessage() - added logging

--- tipsip/sip/transport.py
+++ tipsip/sip/transport.py
@@ -3,6 +3,7 @@
 from collections import namedtuple
 
 from twisted.internet.protocol import DatagramProtocol
+from twisted.python import log
 
 from message import Message
 
@@ -11,19 +12,31 @@ Address = namedtuple('Address', ('host', 'port', 'transport'))
 
 
 class UDPTransport(DatagramProtocol):
-    def __init__(self, listen_interface, messageReceivedCallback):
-        self.messageReceivedCallback = messageReceivedCallback
+    def __init__(self, listen_interface):
+        self.messageReceivedCallback = lambda x: None
         self.listen_interface = listen_interface
 
     def datagramReceived(self, data, (host, port)):
         if not data.strip():
             return
+        log.msg("RECEIVED:\n" + data)
         msg = Message.parse(data)
         msg.received = Address(host, port, "UDP")
         msg.from_interface = self.listen_interface
         self.messageReceivedCallback(msg)
 
     def sendMessage(self, message, host, port):
+        # XXX: do it for requests only
+        self._fillVia(message)
         data = str(message)
-        self.transport.write(data, host, port)
+        log.msg("SENT:\n" + data)
+        self.transport.write(data, (host, port))
+
+    def _fillVia(self, msg):
+        via = msg.headers['via'][0]
+        if via.transport:
+            return
+        via.transport = 'UDP'
+        via.host = self.listen_interface.host
+        via.port = self.listen_interface.port
 
