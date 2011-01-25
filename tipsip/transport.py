991f11013ffcb31cf7560cb08107e0e999b930c1
John Khvatov <ivaxer@sgu.ru>
2011-01-25 13:29:28 +0300

    transport: Changed logging from udp-transport.

--- tipsip/transport.py
+++ tipsip/transport.py
@@ -10,6 +10,9 @@ from message import Message
 
 Address = namedtuple('Address', ('host', 'port', 'transport'))
 
+def debug(msg):
+    if __debug__:
+        log.msg(msg)
 
 class UDPTransport(DatagramProtocol):
     def __init__(self, listen_interface):
@@ -19,7 +22,7 @@ class UDPTransport(DatagramProtocol):
     def datagramReceived(self, data, (host, port)):
         if not data.strip():
             return
-        log.msg("RECEIVED:\n" + data)
+        debug("[UDP] received message from %s:%s:\n%s" % (host, port, data))
         msg = Message.parse(data)
         msg.received = Address(host, port, "UDP")
         msg.from_interface = self.listen_interface
@@ -29,7 +32,7 @@ class UDPTransport(DatagramProtocol):
         # XXX: do it for requests only
         self._fillVia(message)
         data = str(message)
-        log.msg("SENT:\n" + data)
+        debug("[UDP] sent message to %s:%s:\n%s" % (host, port, data))
         self.transport.write(data, (host, port))
 
     def _fillVia(self, msg):
