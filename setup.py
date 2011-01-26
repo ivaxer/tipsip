cfb5e354ef7e42d677b39c988851b1abc26540d3
John Khvatov <ivaxer@imarto.net>
2011-01-26 16:14:00 +0300

    completed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
             name='tipsip',
-            test_suite='tipsip.tests',
+            description='SIP stack on Python',
+            license='MIT',
+            url = 'http://github.com/ivaxer/tipsip',
+            keywords = "SIP VoIP",
+
             author='John Khvatov',
             author_email='ivaxer@imarto.net',
+
+            test_suite='tipsip.tests',
     )
