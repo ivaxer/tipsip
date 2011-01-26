96e112a39d1dd29ad71af34779fb9c885813dba6
John Khvatov <ivaxer@imarto.net>
2011-01-26 18:36:17 +0300

    setup.py: changed description, added 'pakages'

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == '__main__':
     setup(
             name='tipsip',
-            description='SIP stack on Python',
+            description='SIP stack written in Twisted Python',
             license='MIT',
             url = 'http://github.com/ivaxer/tipsip',
             keywords = "SIP VoIP",
 
             author='John Khvatov',
             author_email='ivaxer@imarto.net',
+
+            packages = find_packages(),
 
             test_suite='tipsip.tests',
     )
